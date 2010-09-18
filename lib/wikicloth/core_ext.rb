eb88624c53a70beff5dfe33060d50969791f7588
David Ricciardi <nricciar@david-ricciardis-mac-mini-2.local>
2010-09-17 21:25:41 -0400

    safe math

--- lib/wikicloth/core_ext.rb
+++ lib/wikicloth/core_ext.rb
@@ -19,6 +19,14 @@ class Object
   end
 end
 
+module Math
+  def self.eval(expression)
+    allowed_characters = Regexp.escape('+-*/.() ')
+    safe_expression = expression.match(/[\d#{allowed_characters}]*/).to_s
+    Kernel.eval(safe_expression)
+  end
+end
+
 module ExtendedString
 
   def blank?
