6eb90035fd0df591d00b4039447b2b8f697e2013
David Ricciardi <nricciar@facebook.(none)>
2010-09-23 02:15:30 +0000

    bug fix

--- lib/wikicloth.rb
+++ lib/wikicloth.rb
@@ -57,6 +57,7 @@ module WikiCloth
       self.options[:link_handler].params = options[:params]
       data = self.sections.first.render(self.options)
       data.gsub!(/<!--(.|\s)*?-->/,"")
+      data << "\n" if data.last(1) != "\n"
       buffer = WikiBuffer.new("",options)
       data.each_char { |c| buffer.add_char(c) }
       buffer.to_s
