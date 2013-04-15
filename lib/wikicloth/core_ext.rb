f0fd4de61e6f06c86e584343052740060477153b
Andrei Varanovich <dotnetby@gmail.com>
2013-04-15 12:05:39 +0200

    Fixed external links issues

--- lib/wikicloth/core_ext.rb
+++ lib/wikicloth/core_ext.rb
@@ -47,7 +47,9 @@ module ExtendedString
   end
 
   def auto_link
-    url_check = Regexp.new( '(^|[\n ])([\w]+?://[\w]+[^ \"\r\n\t<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
+    puts "SELF"
+    puts self
+    url_check = Regexp.new( '(\A|[\s]|[\n ]|[\<p\>])([\w]+?://[\w]+[^ \"\r\n\t<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
     www_check = Regexp.new( '(^|[\n ])((www)\.[^ \"\t\n\r<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
     self.gsub!(url_check, '\1<a href="\2">\2</a>')
     self.gsub!(www_check, '\1<a href="http://\2">\2</a>')
