1d675bf7c4f2362d4f3467bdac3a7b47b9a84980
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-04-22 21:17:36 +0200

    retrieve title for media-tags

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
-source :rubygems
+source 'https://rubygems.org'
 gem "pygments.rb", "~> 0.2.13"
+gem "mechanize", "2.6.0"
+gem "json"
 gemspec
--- lib/wikicloth/extensions/media.rb
+++ lib/wikicloth/extensions/media.rb
@@ -1,20 +1,39 @@
 module WikiCloth
   class MediaExtension < Extension
 
+    require 'mechanize'
+    require 'json'
+
     # parser for media urls
     #
     # Youtube: <media url="http://www.youtube.com/watch?v=[_ID_]">
     #
-    element 'media', :skip_html => true do |buffer|
+    element 'media', :skip_html => true, :run_globals => false do |buffer|
 
       # with regexp retrieve id from youtube url
       id = buffer.get_attribute_by_name("url").scan(/.+?\=(.+)/).first.first
 
+      # if retrieved id
       if !id.nil?
+
+        title = 'Title wasn\'t defined'
+        begin
+          # get title of video
+          response = Mechanize.new.get "https://gdata.youtube.com/feeds/api/videos/#{id}?v=2&alt=json"
+          title = JSON.parse response.body['entry']['title']['$t']
+        rescue
+        end
+
         # render html for representing this url as iframe on webpage
-        '<iframe width="420" height="315" src="https://www.youtube-nocookie.com/embed/' + id.to_s + '" frameborder="0" allowfullscreen></iframe>'
+        '<div class="video-title">'+ title +
+            '</div><iframe width="420" height="315" src="https://www.youtube-nocookie.com/embed/' +
+            id.to_s + '" frameborder="0" allowfullscreen></iframe>'
+
       else
-        WikiCloth.error_template 'Cannot provide video =/'
+
+        # else show error message
+        WikiCloth.error_template 'Cannot provide embedded video =/'
+
       end
 
     end
