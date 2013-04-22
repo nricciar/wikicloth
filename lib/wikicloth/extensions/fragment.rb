e16bd500d7a7242b2c24ea1683f767f7787db09d
Andrei Varanovich <dotnetby@gmail.com>
2013-04-22 11:54:56 +0200

    More experiments

--- lib/wikicloth/extensions/fragment.rb
+++ lib/wikicloth/extensions/fragment.rb
@@ -58,7 +58,7 @@ module WikiCloth
           error = "<span class=\"error\">#{err.message}</span>"
         end
       if error.nil?
-        "<div style=\"float:right; margin-right:60px\"><a href='http://101companies.org/resources/#{buffer.element_attributes['url']}?format=html>'>#{buffer.element_attributes['url']}</a></div>#{content}"
+        "<div style=\"float:right; margin-right:60px\">#{url}?format=html>'>#{buffer.element_attributes['url']}</a></div>#{content}"
       else
         error
       end
