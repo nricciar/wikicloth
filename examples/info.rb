1cdb5b4f34a9aa8965a6823ea4971d5e39c1c5a5
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2014-02-25 16:26:38 +0100

    handle section title for sub-resources

--- examples/info.rb
+++ examples/info.rb
@@ -2,23 +2,15 @@ require File.join(File.dirname(__FILE__),'../init.rb')
 if ARGV[0] && File.exists?(ARGV[0])
   data = File.read(ARGV[0])
 else
-  data = "== Section ==
+  data = "== @Section ==
 dqwd
-=== Some Stuff ===
-dqwdq
-==== More stuff ====
-
-== Foo ==
-
-dqdq
-
 "
 end
 
 wiki = WikiCloth::Parser.new(:data => data)
 #puts wiki.to_html
 #puts "..."
-puts wiki.sections.first.children.length
+puts wiki.to_html
 
 # puts "Internal Links: #{wiki.internal_links.size}"
 # puts "External Links: #{wiki.external_links.size}"
