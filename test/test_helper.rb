d565017e953941843c2acae1a27e1d654f0d0daf
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-26 19:22:03 +0800

    Delete test directory

--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'test/unit'
-require 'active_support'
-I18n.load_path << File.join(File.dirname(__FILE__),'locales.yml')
-require File.join(File.dirname(__FILE__),'../init')
